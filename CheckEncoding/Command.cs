0ab7fff3d15a90027b1e71280a6ac8421f9ba48d
Rostislav Listerenko <listerenko@mindbox.ru>
2020-06-05 17:39:58 +0300

    action

---
+++ CheckEncoding/Command.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CommandLine;
+using Ude;
+
+#pragma warning disable CS8618
+
+namespace CheckEncoding
+{
+	internal abstract class Command
+	{
+		[Option('e', "target-encoding", Default = "utf-8")]
+		public string TargetEncoding { get; set; }
+	    
+		[Option("extensions", Default = "txt,xml,json,config,yaml,yml,cs,csproj,sln", Separator = ',')]
+		public IEnumerable<string> FileExtensions { get; set; }
+
+		[Option('d', "directory", Required = true)]
+		public string TargetDirectory { get; set; }
+	    
+		public abstract CommandResult Execute();
+
+		protected (string path, bool isValid, string encoding)[] GetFilesWithWrongEncoding()
+		{
+			var extensions = new HashSet<string>(
+				FileExtensions.Select(extensionWithoutDot => "." + extensionWithoutDot), 
+				StringComparer.OrdinalIgnoreCase);
+		    
+			var filesToCheck = Directory
+				.EnumerateFiles(TargetDirectory, "*", SearchOption.AllDirectories)
+				.Where(filePath => extensions.Contains(Path.GetExtension(filePath)));
+
+			return filesToCheck
+				.Select(file =>
+				{
+					var detector = new CharsetDetector();
+					using var fileStream = File.Open(file, FileMode.Open);
+					
+					detector.Feed(fileStream);
+					detector.DataEnd();
+
+					var isValidEncoding =
+						detector.Charset.Equals(TargetEncoding, StringComparison.InvariantCultureIgnoreCase);
+					
+					return (path: file, isValid: isValidEncoding, encoding: detector.Charset);
+				})
+				.Where(file => !file.isValid)
+				.ToArray();
+		}
+	}
+}
